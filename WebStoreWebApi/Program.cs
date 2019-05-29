8c1b14b9949e95de85e164c307d4a86ed9ed7d48
Jack Yen <jack.yen@getac.com.tw>
2019-05-29 13:14:01 +0800

    Add function to show and save the log in Azure
    When the application is deployed in Azure app service, you can see
    the log in diagnostics log and save it in blob.

--- WebStoreWebApi/Program.cs
+++ WebStoreWebApi/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+//using Microsoft.Extensions.Logging.AzureAppServices;
 using NLog.Web;
 
 namespace WebStoreWebApi
@@ -14,13 +16,29 @@ namespace WebStoreWebApi
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                })
-                .UseNLog();  // NLog: setup NLog for Dependency injection
+                WebHost.CreateDefaultBuilder(args)
+                    .UseStartup<Startup>()
+                    .ConfigureLogging(logging =>
+                    {
+                        logging.ClearProviders();
+                        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                    })
+                    .UseNLog();  // NLog: setup NLog for Dependency injection
+                
+                // Add log in Azure diagnostics logs
+
+                /*WebHost.CreateDefaultBuilder(args)
+                    .ConfigureLogging(logging => logging.AddAzureWebAppDiagnostics())
+                    .ConfigureServices(serviceCollection => serviceCollection
+                            .Configure<AzureFileLoggerOptions>(options =>
+                            {
+                                options.FileName = "azure-diagnostics-";
+                                options.FileSizeLimit = 50 * 1024;
+                                options.RetainedFileCountLimit = 5;
+                            }).Configure<AzureBlobLoggerOptions>(options =>
+                            {
+                                options.BlobName = "log.txt";
+                            }))
+                    .UseStartup<Startup>();*/
     }
 }
